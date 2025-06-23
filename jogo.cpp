f9da4ce1f21f1f15909002dad054c15f4a92c013
Carlos Henrique Deucher <102810715+carlosDeucher@users.noreply.github.com>
2025-06-23 02:43:05 +0000

    feat: obtem e exibe o palpite / dev mode

--- jogo.cpp
+++ jogo.cpp
@@ -3,27 +3,35 @@
 using namespace std;
 
 #define TAM 4
+#define DEV_MODE true
 
 int main() {
     srand(time(0));
 
-    int matriz[TAM][TAM]={1,4,5,2,7,2,8,7,3,6,1,4,6,5,3,8}, matrizGabarito[TAM][TAM], matrizJogo[TAM][TAM], i, j, k=0, l;
+    int matriz[TAM][TAM]={1,4,5,2,7,2,8,7,3,6,1,4,6,5,3,8}, matrizGabarito[TAM][TAM], matrizJogo[TAM][TAM], i, j, k=0, l, primeiroPalpiteLinha, primeiroPalpiteColuna,
+        segundoPalpiteLinha, segundoPalpiteColuna;
 
 
     // impressao matriz principal
-
     cout << "Matriz Principal:" << endl;
     for (i = 0; i < TAM; i++) {
         for (j = 0; j < TAM; j++) {
-            cout << matriz[i][j] << " ";
+            cout << matriz[i][j] << "\t";
         }
         cout << endl;
+    }
+    
+    if(DEV_MODE) {
+        cout << "DEV MODE!!!" << endl;
     }
 
     switch(rand() % 4 + 1) {
         case 1:
             // matriz original (CERTA)
-            cout << "Matriz Original:" << endl;
+            if(DEV_MODE) {
+            cout << "Matriz Original:" << endl;    
+            }
+            
             for (i = 0; i < TAM; i++) {
                 for (j = 0; j < TAM; j++) {
                     matrizGabarito[i][j] = matriz[i][j];
@@ -32,7 +40,10 @@ int main() {
             break;
         case 2:
             // matriz transposta (CERTA)
-            cout << "Matriz Transposta:" << endl;
+            if(DEV_MODE) {
+            cout << "Matriz Transposta:" << endl;    
+            }
+            
             for (i = 0; i < TAM; i++) {
                 for (j = 0; j < TAM; j++) {
                     matrizGabarito[j][i] = matriz[i][j];
@@ -41,35 +52,72 @@ int main() {
             break;
         case 3:
             // invertida por linha (CERTA)
-            cout<< "Matriz Invertida por Linha:" << endl;
+            if(DEV_MODE) {
+            cout << "Matriz Invertida por Linha:" << endl;    
+            }
+            
             for (i = TAM - 1; i >= 0 ; i--) {
                 for (j = 0; j < TAM; j++) {
                     matrizGabarito[i][j] = matriz[k][j];
-                    
                 }
                 k++;
             }
             break;
         case 4:
             // invertida por coluna
-            cout << "Matriz Invertida por Coluna:" << endl;
+            if(DEV_MODE) {
+            cout << "Matriz Invertida por Coluna:" << endl;    
+            }
+            
             for (i = 0; i < TAM; i++) {
                 k=TAM -1;
                 for (j = 0; j < TAM; j++) {
                     matrizGabarito[i][j] = matriz[i][k];
                     k--;
                 }
+            }
+            break;
     }
-
-    cout << "Matriz Gabarito:" << endl;
+    
+    // Printa a matriz gabarito no modo dev_mode
+    if(DEV_MODE) {
     for (i = 0; i < TAM; i++) {
         for (j = 0; j < TAM; j++) {
-            cout << matrizGabarito[i][j] << " ";
+            cout << matrizGabarito[i][j] << '\t';
         }
-        cout << endl;
-    } 
+    cout << endl;
+    }
+    }
     
     
-
+    // Início do jogo
+    do {
+    // Obtém a primeira coordenada do palpite
+    cout << "Digite a primeira coordenada: " << endl << "X x X: ";
+    cin >> primeiroPalpiteLinha;
+    cout << primeiroPalpiteLinha << " x X: ";
+    cin >> primeiroPalpiteColuna;
+    cout << primeiroPalpiteLinha << " x " << primeiroPalpiteColuna << endl;
+    
+    // Obtém a segunda coordenada do palpite
+    cout << "Digite a segunda coordenada: " << endl << "X x X: ";
+    cin >> segundoPalpiteLinha;
+    cout << segundoPalpiteLinha << " x X: ";
+    cin >> segundoPalpiteColuna;
+    cout << segundoPalpiteLinha << " x " << segundoPalpiteColuna << endl << endl;
+    
+    // Printa a matriz com as coordenadas escolhidas
+    for (i = 0; i < TAM; i++) {
+        for (j = 0; j < TAM; j++) {
+            if((primeiroPalpiteLinha == (i + 1)  and primeiroPalpiteColuna == (j + 1)) or (segundoPalpiteLinha == (i + 1) and segundoPalpiteColuna == (j + 1))) {
+            cout << matrizGabarito[i][j] << '\t';    
+            } else {
+            cout << "X" << '\t';
+            }
+        }
+    cout << endl;
+    }
+    } while(true);
+    
     return 0;
 }
